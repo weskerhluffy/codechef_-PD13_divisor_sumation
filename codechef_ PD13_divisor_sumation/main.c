d1fffc103cf7243d9c80bef25f636e9ec394e261
ernesto.alvarado <ernesto.alvarado@adroll.com>
2019-12-13 17:42:02 -0600

    Initial Commit

---
+++ codechef_ PD13_divisor_sumation/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  codechef_ PD13_divisor_sumation
+//
+//  Created by ernesto alvarado on 13/12/19.
+//  Copyright © 2019 ernesto alvarado. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
